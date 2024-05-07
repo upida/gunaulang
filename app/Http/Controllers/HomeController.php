e5e8bfad8c044c52829fbd5514ce45eb2e155255
upida <salmamufida34@gmail.com>
2024-05-07 23:04:57 +0700

    home: complete categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,24 +40,106 @@ class HomeController extends Controller
             $today = date('Y-m-d');
 
             // product.free_food
-            // $data['product']['free_food'] = Product::where([
-            //     ['stock', '>', 0],
-            //     ['is_food', '=', true],
-            //     ['price', '=', 0],
-            //     ['expired_at', '>', $today],
-            // ])
-            // ->orderBy('created_at', 'desc')
-            // ->limit(10);
+            $data['product']['free_food'] = Product::select('products.*')
+            ->select('stores.*');
+
+            if (!empty($data['address']['latitude']) && !empty($data['address']['longitude'])) {
+                $data['product']['free_food'] = $data['product']['free_food']
+                ->selectRaw('
+                    (
+                        6371 * 1000 * acos(
+                            cos(radians(?))
+                            * cos(radians(stores.latitude)) 
+                            * cos(radians(stores.longitude) - radians(?)) 
+                            + sin(radians(?)) * sin(radians(stores.latitude))
+                        )
+                    ) AS distance', 
+                    [
+                        $data['address']['latitude'],
+                        $data['address']['longitude'],
+                        $data['address']['latitude']
+                    ]
+                );
+            } 
             
+            $data['product']['free_food'] = $data['product']['free_food']
+            ->join('stores', 'products.store_id', '=', 'stores.id');
+
+            if (
+                empty($data['address']['latitude']) &&
+                empty($data['address']['longitude']) &&
+                !empty($data['address']['province'])
+            ) {
+                $data['product']['free_food'] = $data['product']['free_food']
+                ->where('stores.province', '=', $data['address']['province']);
+            }
+
+            $data['product']['free_food'] = $data['product']['free_food']->where([
+                ['products.stock', '>', 0],
+                ['products.is_new', '=', true],
+                ['products.is_food', '=', true],
+                ['products.price', '=', 0],
+                ['products.expired_at', '>=', $today],
+            ]);
+            
+            if (!empty($data['address']['latitude']) && !empty($data['address']['longitude'])) {
+                $data['product']['free_food'] = $data['product']['free_food']->orderBy('distance');
+            }
+
+            $data['product']['free_food'] = $data['product']['free_food']->orderBy('products.created_at', 'desc');
+            $data['product']['free_food'] = $data['product']['free_food']->limit(10);
+            $data['product']['free_food'] = $data['product']['free_food']->get()->toArray();
+
             // product.cheap_food
-            // $data['product']['cheap_food'] = Product::where([
-            //     ['stock', '>', 0],
-            //     ['is_food', '=', true],
-            //     ['price', '>', 0],
-            //     ['expired_at', '>', $today],
-            // ])
-            // ->orderBy('created_at', 'desc')
-            // ->limit(10);
+            $data['product']['cheap_food'] = Product::select('products.*')
+            ->select('stores.*');
+
+            if (!empty($data['address']['latitude']) && !empty($data['address']['longitude'])) {
+                $data['product']['cheap_food'] = $data['product']['cheap_food']
+                ->selectRaw('
+                    (
+                        6371 * 1000 * acos(
+                            cos(radians(?))
+                            * cos(radians(stores.latitude)) 
+                            * cos(radians(stores.longitude) - radians(?)) 
+                            + sin(radians(?)) * sin(radians(stores.latitude))
+                        )
+                    ) AS distance', 
+                    [
+                        $data['address']['latitude'],
+                        $data['address']['longitude'],
+                        $data['address']['latitude']
+                    ]
+                );
+            } 
+            
+            $data['product']['cheap_food'] = $data['product']['cheap_food']
+            ->join('stores', 'products.store_id', '=', 'stores.id');
+
+            if (
+                empty($data['address']['latitude']) &&
+                empty($data['address']['longitude']) &&
+                !empty($data['address']['province'])
+            ) {
+                $data['product']['cheap_food'] = $data['product']['cheap_food']
+                ->where('stores.province', '=', $data['address']['province']);
+            }
+
+            $data['product']['cheap_food'] = $data['product']['cheap_food']->where([
+                ['products.stock', '>', 0],
+                ['products.is_new', '=', true],
+                ['products.is_food', '=', true],
+                ['products.price', '>', 0],
+                ['products.expired_at', '>=', $today],
+            ]);
+            
+            if (!empty($data['address']['latitude']) && !empty($data['address']['longitude'])) {
+                $data['product']['cheap_food'] = $data['product']['cheap_food']->orderBy('distance');
+            }
+
+            $data['product']['cheap_food'] = $data['product']['cheap_food']->orderBy('products.created_at', 'desc');
+            $data['product']['cheap_food'] = $data['product']['cheap_food']->limit(10);
+            $data['product']['cheap_food'] = $data['product']['cheap_food']->get()->toArray();
             
             // product.food_waste
             $data['product']['food_waste'] = Product::select('products.*')
@@ -112,12 +194,53 @@ class HomeController extends Controller
             $data['product']['food_waste'] = $data['product']['food_waste']->get()->toArray();
             
             // product.processed_waste
-            // $data['product']['processed_waste'] = Product::where([
-            //     ['stock', '>', 0],
-            //     ['is_food', '=', false],
-            // ])
-            // ->orderBy('created_at', 'desc')
-            // ->limit(10);
+            $data['product']['processed_waste'] = Product::select('products.*')
+            ->select('stores.*');
+
+            if (!empty($data['address']['latitude']) && !empty($data['address']['longitude'])) {
+                $data['product']['processed_waste'] = $data['product']['processed_waste']
+                ->selectRaw('
+                    (
+                        6371 * 1000 * acos(
+                            cos(radians(?))
+                            * cos(radians(stores.latitude)) 
+                            * cos(radians(stores.longitude) - radians(?)) 
+                            + sin(radians(?)) * sin(radians(stores.latitude))
+                        )
+                    ) AS distance', 
+                    [
+                        $data['address']['latitude'],
+                        $data['address']['longitude'],
+                        $data['address']['latitude']
+                    ]
+                );
+            } 
+            
+            $data['product']['processed_waste'] = $data['product']['processed_waste']
+            ->join('stores', 'products.store_id', '=', 'stores.id');
+
+            if (
+                empty($data['address']['latitude']) &&
+                empty($data['address']['longitude']) &&
+                !empty($data['address']['province'])
+            ) {
+                $data['product']['processed_waste'] = $data['product']['processed_waste']
+                ->where('stores.province', '=', $data['address']['province']);
+            }
+
+            $data['product']['processed_waste'] = $data['product']['processed_waste']->where([
+                ['products.stock', '>', 0],
+                ['products.is_new', '=', true],
+                ['products.is_food', '=', false],
+            ]);
+            
+            if (!empty($data['address']['latitude']) && !empty($data['address']['longitude'])) {
+                $data['product']['processed_waste'] = $data['product']['processed_waste']->orderBy('distance');
+            }
+
+            $data['product']['processed_waste'] = $data['product']['processed_waste']->orderBy('products.created_at', 'desc');
+            $data['product']['processed_waste'] = $data['product']['processed_waste']->limit(10);
+            $data['product']['processed_waste'] = $data['product']['processed_waste']->get()->toArray();
 
             return Inertia::render('Home', [
                 'canLogin' => Route::has('login'),
