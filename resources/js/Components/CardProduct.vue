e97e1e9ff67ff30b2dc8bccf29793230e6cad746
rizahoemae <humaida.mawaddatur@gmail.com>
2024-05-10 01:25:27 +0700

    adjustment card product: change price to distance, etc

--- resources/js/Components/CardProduct.vue
+++ resources/js/Components/CardProduct.vue
@@ -14,47 +14,42 @@ const props = defineProps({
     description: {
         type: String,
     },
-    price: {
+    distance: {
         type: [String, Number],
     },
     store_name: {
         type: String,
     },
+    image: {
+        type: Array,
+    },
 });
-const moneyFormat = (args) => {
-    if (args !== null && args !== undefined && args > 0) {
-        args = Math.round(args);
-
-        // Format the input value in Indonesian currency format
-        return args.toLocaleString("id-ID");
+const distanceFormat = (args) => {
+    if (args) {
+        if (args < 1000) {
+            return `${args} M`;
+        }
+        return `${Math.round(args / 1000)} KM`;
     }
-    return 0;
+    return `0 M`;
 };
 </script>
 <template>
     <v-card class="" max-width="400">
         <v-img
-            class="align-end text-white"
+            class="text-white align-end"
             height="200"
-            :src="props.image"
             cover
+            :src="`${route().t.url}/storage/${props.image?.[0]?.path}`"
         >
-            <!-- <v-card-title
-                                >Top 10 Australian beaches</v-card-title
-                            > -->
         </v-img>
 
         <v-card-title class="pt-4"> {{ props.name }} </v-card-title>
-        <v-card-subtitle class="pt-4"
-            >Rp {{ moneyFormat(props.price) }}</v-card-subtitle
-        >
-
         <v-card-text>
-            <div>{{ props.description }}</div>
-            <div
-                v-if="props.store_name"
-                class="text-green-600 items-center flex cursor-pointer"
-            >
+            <div>
+                {{ distanceFormat(props.distance) }}
+            </div>
+            <div class="text-green-600 items-center flex cursor-pointer">
                 <v-icon icon="mdi-store-outline" class="mr-2"></v-icon
                 >{{ props.store_name }}
             </div>
