b0359629edb3f852371ac5f91fdfc5b643e2375c
rizahoemae <humaida.mawaddatur@gmail.com>
2024-05-10 14:36:59 +0700

    feat & adjustment: slicing order payment, callback, and detail

--- resources/js/Pages/Cart/Index.vue
+++ resources/js/Pages/Cart/Index.vue
@@ -55,56 +55,76 @@ const updateCart = (id, quantity) => {
         });
 };
 
+// const checkout = () => {
+//     // Initialize an array to store the transformed data
+//     const transformedData = [];
+
+//     // Iterate through each store in the cart
+//     for (const storeName in usePage().props.data.cart) {
+//         const store = usePage().props.data.cart[storeName];
+//         let total = 0;
+
+//         // Initialize an array to store selected products for this store
+//         const selectedProducts = [];
+
+//         // Iterate through each product in the store
+//         for (const product of store.product) {
+//             // Check if the product is selected
+//             if (product.selected) {
+//                 // Calculate the total for this product (quantity * price)
+//                 const productTotal = product.quantity * product.price;
+//                 // Add the product total to the store total
+//                 total += productTotal;
+//                 // Add the selected product to the selectedProducts array
+//                 selectedProducts.push({
+//                     id: product.id,
+//                     title: product.title,
+//                     quantity: product.quantity,
+//                     price: product.price,
+//                 });
+//             }
+//         }
+
+//         // If there are selected products for this store, add them to the transformedData array
+//         if (selectedProducts.length > 0) {
+//             transformedData.push({
+//                 store: {
+//                     id: store.store.id,
+//                 },
+//                 total: {
+//                     all: total,
+//                 },
+//                 products: selectedProducts,
+//             });
+//         }
+//     }
+//     const payload = JSON.stringify({ products: transformedData }, null, 2);
+//     axios
+//         .post(`${route().t.url}/order/add`, payload)
+//         .then(function (response) {
+//             toast.checkout = true;
+//         })
+//         .catch(function (error) {});
+// };
+
 const checkout = () => {
-    // Initialize an array to store the transformed data
-    const transformedData = [];
-
-    // Iterate through each store in the cart
-    for (const storeName in usePage().props.data.cart) {
-        const store = usePage().props.data.cart[storeName];
-        let total = 0;
-
-        // Initialize an array to store selected products for this store
-        const selectedProducts = [];
-
-        // Iterate through each product in the store
-        for (const product of store.product) {
-            // Check if the product is selected
+    const selectedProducts = [];
+    for (const store in usePage().props.data.cart) {
+        const products = usePage().props.data.cart[store].product;
+        for (const product of products) {
             if (product.selected) {
-                // Calculate the total for this product (quantity * price)
-                const productTotal = product.quantity * product.price;
-                // Add the product total to the store total
-                total += productTotal;
-                // Add the selected product to the selectedProducts array
                 selectedProducts.push({
                     id: product.id,
-                    title: product.title,
                     quantity: product.quantity,
-                    price: product.price,
                 });
             }
         }
-
-        // If there are selected products for this store, add them to the transformedData array
-        if (selectedProducts.length > 0) {
-            transformedData.push({
-                store: {
-                    id: store.store.id,
-                },
-                total: {
-                    all: total,
-                },
-                products: selectedProducts,
-            });
-        }
     }
-    const payload = JSON.stringify({ products: transformedData }, null, 2);
-    axios
-        .post(`${route().t.url}/order/add`, payload)
-        .then(function (response) {
-            toast.checkout = true;
-        })
-        .catch(function (error) {});
+
+    const payload = {
+        products: selectedProducts,
+    };
+    router.post("/order", payload);
 };
 const sumTotal = computed(() => {
     let total = 0;
@@ -312,222 +332,31 @@ onMounted(() => {
     </BasicLayout>
     <BasicLayout :canLogin="canLogin" :canRegister="canRegister">
         <div class="py-12">
-            <div class="max-w-7xl mx-auto px-6 lg:px-8 space-y-5">
-                <h1 class="font-bold text-lg text-uppercase">Pemesanan</h1>
-
-                <h2 class="font-semibold text-md">Lokasi Penerima</h2>
-                <Address />
-
-                <h2 class="font-semibold text-md">Detail Pesanan</h2>
-
-                <div class="space-y-6">
-                    <v-card
-                        v-for="cart in Object.keys(data.cart)"
-                        class="w-full py-2"
-                    >
-                        <v-card-item>
-                            <v-card-title>
-                                <div
-                                    class="w-full justify-between flex items-center"
-                                >
-                                    <p>
-                                        {{ cart }}
-                                    </p>
-                                    <p class="text-sm">#5km</p>
-                                </div>
-                                <!-- <v-avatar class="mr-3">
-                                    <v-img
-                                        alt="John"
-                                        src="https://cdn.vuetifyjs.com/images/john.jpg"
-                                    ></v-img> </v-avatar
-                                > -->
-                            </v-card-title>
-                        </v-card-item>
-                        <v-divider></v-divider>
-                        <v-card-text>
-                            <v-list lines="two">
-                                <v-list-item
-                                    v-for="item in data.cart[cart].product"
-                                    rounded="0"
-                                    class="items-center"
-                                >
-                                    <template v-slot:title>
-                                        <div
-                                            class="flex sm:flex-row flex-col items-center sm:justify-between"
-                                        >
-                                            <div class="flex items-center">
-                                                <!-- <div>
-                                                    <v-img
-                                                        width="50"
-                                                        height="50"
-                                                        class=""
-                                                        alt="John"
-                                                        src="https://cdn.vuetifyjs.com/images/john.jpg"
-                                                    ></v-img>
-                                                </div> -->
-                                                <p class="ml-4">
-                                                    {{ item.title }}
-                                                </p>
-                                            </div>
-                                            <div
-                                                class="flex items-center space-x-5"
-                                            >
-                                                <p>
-                                                    Rp
-                                                    {{
-                                                        moneyFormat(item.price)
-                                                    }}
-                                                </p>
-                                                <p>x {{ item.quantity }}</p>
-                                            </div>
-                                        </div>
-                                    </template>
-                                </v-list-item>
-
-                                <v-divider inset></v-divider>
-                            </v-list>
-                        </v-card-text>
-                        <v-divider></v-divider>
-                        <v-card-actions class="flex justify-end space-x-5 px-8">
-                            <h1 class="font-bold">Total:</h1>
-                            <p>Rp {{ moneyFormat(0) }}</p>
-                        </v-card-actions>
-                    </v-card>
-                    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <h2 class="font-semibold text-md">
-                            Merasa Terbantu? Kirim donasi untuk layanan kami
-                        </h2>
-
-                        <TextInput class="w-full" />
-                    </div>
-                    <h2 class="font-semibold text-md">Detail Pembayaran</h2>
-
-                    <div
-                        class="p-4 sm:p-8 bg-white shadow sm:rounded-lg grid grid-cols-2 gap-2 items-center"
-                    >
-                        <p>Subtotal</p>
-                        <p class="text-right">Rp {{ moneyFormat(sumTotal) }}</p>
-                        <p>Donasi</p>
-                        <p class="text-right">Rp {{ moneyFormat(sumTotal) }}</p>
-                        <p class="font-bold">Total</p>
-                        <p class="text-right font-bold">
-                            Rp {{ moneyFormat(sumTotal) }}
-                        </p>
-                    </div>
-                    <PrimaryButton
-                        @click="checkout"
-                        class="w-full py-3 flex justify-center items-center"
-                    >
-                        Bayar sekarang
-                    </PrimaryButton>
-                </div>
-            </div>
-        </div>
-    </BasicLayout>
-    <BasicLayout :canLogin="canLogin" :canRegister="canRegister">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto px-6 lg:px-8 space-y-5">
-                <h1 class="font-bold text-lg text-uppercase">Pembayaran</h1>
-                <div
-                    class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex flex-col space-y-5 justify-center items-center"
-                >
-                    <div
-                        class="border-4 border-gray-500 p-2 rounded-full w-14 h-14 flex justify-center items-center"
-                    >
-                        <v-icon
-                            icon="mdi-exclamation"
-                            class="text-gray-500"
-                        ></v-icon>
-                    </div>
-                    <p class="text-gray-400">
-                        Anda telah melakukan transaksi sejumlah Rp
-                        {{ moneyFormat(0) }}
-                    </p>
-                    <h2 class="font-semibold text-md items-start w-full">
-                        Metode Pembayaran
-                    </h2>
-                    <v-list class="w-full">
-                        <v-list-group value="Bank">
-                            <template v-slot:activator="{ props }">
-                                <v-list-item
-                                    v-bind="props"
-                                    prepend-icon="mdi-bank"
-                                    title="Bank"
-                                ></v-list-item>
-                            </template>
-                            <v-list-item
-                                v-for="(title, i) in [
-                                    'BCA',
-                                    'BNI',
-                                    'BSI',
-                                    'Mandiri',
-                                ]"
-                                :key="i"
-                                :title="title"
-                                :value="title"
-                            ></v-list-item>
-                        </v-list-group>
-                        <v-list-group value="E-wallet">
-                            <template v-slot:activator="{ props }">
-                                <v-list-item
-                                    v-bind="props"
-                                    prepend-icon="mdi-account"
-                                    title="E-wallet"
-                                ></v-list-item>
-                            </template>
-                            <v-list-item
-                                v-for="(title, i) in [
-                                    'Gopay',
-                                    'OVO',
-                                    'Shopeepay',
-                                ]"
-                                :key="i"
-                                :title="title"
-                                :value="title"
-                            ></v-list-item>
-                        </v-list-group>
-                    </v-list>
-                    <PrimaryButton
-                        @click="payNow"
-                        class="ms-4 text-center justify-center flex"
-                        :class="{ 'opacity-25': form.processing }"
-                        :disabled="selectedProduct == 0"
-                    >
-                        Lakukan Pembayaran
-                    </PrimaryButton>
-                </div>
-            </div>
-        </div>
-    </BasicLayout>
-    <BasicLayout :canLogin="canLogin" :canRegister="canRegister">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto px-6 lg:px-8 space-y-5">
-                <h1 class="font-bold text-lg text-uppercase">
-                    Status Pembayaran
+            <div class="max-w-7xl mx-auto px-6 lg:px-8">
+                <h1 class="font-bold text-lg mb-5 text-uppercase">
+                    Alamat Pengiriman
                 </h1>
-                <div
-                    class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex flex-col space-y-5 justify-center items-center"
-                >
+                <h1 class="font-bold text-lg mb-5 text-uppercase">
+                    Informasi Toko
+                </h1>
+                <Address />
+                <div class="space-y-6">
                     <div
-                        class="border-4 border-green-500 p-2 rounded-full w-14 h-14 flex justify-center items-center"
+                        class="px-4 py-3 sm:p-8 bg-white shadow sm:rounded-lg flex justify-end space-x-2"
                     >
-                        <v-icon
-                            icon="mdi-check"
-                            class="text-green-500"
-                        ></v-icon>
+                        <div class="text-end">
+                            <p class="font-semibold text-lg">Total</p>
+                            <p class="">Rp {{ moneyFormat(sumTotal) }}</p>
+                        </div>
+                        <PrimaryButton
+                            @click="checkout"
+                            class="ms-4 text-center justify-center flex"
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="selectedProduct == 0"
+                        >
+                            Pesan sekarang ({{ selectedProduct }})
+                        </PrimaryButton>
                     </div>
-                    <p class="text-gray-400">
-                        Anda telah berhasil melakukan pembayaran sejumlah Rp
-                        {{ moneyFormat(0) }}
-                    </p>
-                    <PrimaryButton
-                        @click="payNow"
-                        class="ms-4 text-center justify-center flex"
-                        :class="{ 'opacity-25': form.processing }"
-                        :disabled="selectedProduct == 0"
-                    >
-                        Lihat Detail Transaksi
-                    </PrimaryButton>
                 </div>
             </div>
         </div>
---
+++ resources/js/Pages/Order/Payment/Demo.vue
@@ -0,0 +1,182 @@
+<script setup>
+import { Head, Link, useForm, router, usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+import BasicLayout from "@/Layouts/BasicLayout.vue";
+import TextInput from "@/Components/TextInput.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import InputError from "@/Components/InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import Toggle from "@/Components/Toggle.vue";
+import Address from "@/Components/Address.vue";
+import { onMounted, reactive, ref } from "vue";
+import axios from "axios";
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    data: {
+        type: Object,
+    },
+});
+
+const form = useForm({
+    is_active: false,
+    name: "",
+    phone: "",
+    address: "",
+    subdistrict: "",
+    district: "",
+    city: "",
+    province: "",
+    latitude: "",
+    longitude: "",
+    gmaps_point: "",
+    notes: "",
+});
+const toast = reactive({
+    edit: false,
+    checkout: false,
+});
+
+const payNow = () => {
+    console.log(usePage().props.data);
+    const payload = {
+        products: usePage().props.data.products.map((pr) => {
+            return {
+                store: {
+                    id: pr.store.id,
+                },
+                total: pr.total,
+                products: pr.products.map((item) => {
+                    return {
+                        id: item.id,
+                        title: item.title,
+                        quantity: item.quantity,
+                        price: item.price,
+                    };
+                }),
+            };
+        }),
+        address: usePage().props.data.address,
+        donate: donate.value,
+    };
+    axios
+        .post(`${route().t.url}/order/add`, payload)
+        .then(function (response) {
+            toast.checkout = true;
+        })
+        .catch(function (error) {});
+};
+
+const donate = ref(null);
+const moneyFormat = (args) => {
+    if (args !== null && args !== undefined && args > 0) {
+        args = Math.round(args);
+
+        // Format the input value in Indonesian currency format
+        return args.toLocaleString("id-ID");
+    }
+    return 0;
+};
+
+onMounted(() => {});
+</script>
+
+<template>
+    <Head title="Setiap Bagian Berharga" />
+    <BasicLayout :canLogin="canLogin" :canRegister="canRegister">
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto px-6 lg:px-8 space-y-5">
+                <h1 class="font-bold text-lg text-uppercase">Pembayaran</h1>
+                <div
+                    class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex flex-col space-y-5 justify-center items-center"
+                >
+                    <div
+                        class="border-4 border-gray-500 p-2 rounded-full w-14 h-14 flex justify-center items-center"
+                    >
+                        <v-icon
+                            icon="mdi-exclamation"
+                            class="text-gray-500"
+                        ></v-icon>
+                    </div>
+                    <p class="text-gray-400">
+                        Anda telah melakukan transaksi sejumlah Rp
+                        {{ moneyFormat(data.total) }}
+                    </p>
+                    <h2 class="font-semibold text-md items-start w-full">
+                        Metode Pembayaran
+                    </h2>
+                    <v-list class="w-full">
+                        <v-list-group value="Bank">
+                            <template v-slot:activator="{ props }">
+                                <v-list-item
+                                    v-bind="props"
+                                    prepend-icon="mdi-bank"
+                                    title="Bank"
+                                ></v-list-item>
+                            </template>
+                            <v-list-item
+                                v-for="(title, i) in [
+                                    'BCA',
+                                    'BNI',
+                                    'BSI',
+                                    'Mandiri',
+                                ]"
+                                :key="i"
+                                :title="title"
+                                :value="title"
+                            ></v-list-item>
+                        </v-list-group>
+                        <v-list-group value="E-wallet">
+                            <template v-slot:activator="{ props }">
+                                <v-list-item
+                                    v-bind="props"
+                                    prepend-icon="mdi-account"
+                                    title="E-wallet"
+                                ></v-list-item>
+                            </template>
+                            <v-list-item
+                                v-for="(title, i) in [
+                                    'Gopay',
+                                    'OVO',
+                                    'Shopeepay',
+                                ]"
+                                :key="i"
+                                :title="title"
+                                :value="title"
+                            ></v-list-item>
+                        </v-list-group>
+                    </v-list>
+                    <PrimaryButton
+                        @click="payNow"
+                        class="ms-4 text-center justify-center flex"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="selectedProduct == 0"
+                    >
+                        Lakukan Pembayaran
+                    </PrimaryButton>
+                </div>
+            </div>
+        </div>
+    </BasicLayout>
+    <v-snackbar v-model="toast.edit">
+        Berhasil mengubah jumlah
+        <template v-slot:actions>
+            <v-btn color="pink" variant="text" @click="toast.edit = false">
+                Tutup
+            </v-btn>
+        </template>
+    </v-snackbar>
+    <v-snackbar v-model="toast.checkout">
+        Berhasil checkout
+        <template v-slot:actions>
+            <v-btn color="pink" variant="text" @click="toast.edit = false">
+                Tutup
+            </v-btn>
+        </template>
+    </v-snackbar>
+</template>
